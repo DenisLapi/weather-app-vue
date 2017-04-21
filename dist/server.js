c58270fbd313dca18637af3d7ca1b94ad38ff9b3
DenisLapi <denislapidenis@gmail.com>
2017-04-22 00:50:04 +0200

    index.html

--- dist/server.js
+++ dist/server.js
@@ -10,4 +10,7 @@ app.listen(port);
 
 console.log('server started '+ port);
 
-app.get('/', function(req, res) { res.send('Hello world'); })
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
